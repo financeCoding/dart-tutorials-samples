05b39abbefb4eed02d129dbc4b2527dc3178c60b
adam <financeCoding@gmail.com>
2013-04-18 15:31:30 -0700

    minor tweeks

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -13,9 +13,36 @@ void main() {
   // If the project has web components then call build.dart
 
   // Copy the files from the web folders to output/target<X>/<sample_name>/web/
-
+  //addAsyncTask('deploy', (ctx) => startProcess(ctx, 'rsync', ['-RLr', 'web', 'output/']));
   // Call the publish gh-pages branch from hop.
-  addAsyncTask('deploy_gh_pages', (ctx) => branchForDir(ctx, 'master', 'output', 'gh-pages'));
+  //addAsyncTask('deploy_gh_pages', (ctx) => branchForDir(ctx, 'master', 'output/web', 'gh-pages'));
+
+  // TODO: add a future wait all tasks
+
+  String deployFolderName = 'deploy';
+
+  addAsyncTask('deploy', (ctx) => startProcess(ctx, 'rsync', ['-Rr', 'web', deployFolderName]));
+
+  addTask('dart2js',
+      createDart2JsTask(['web/target01/clickme/web/clickme.dart'],
+      output: '${deployFolderName}/web/target01/clickme/web/clickme.dart.js',
+      minify: true,
+      liveTypeAnalysis: true,
+      rejectDeprecatedFeatures: true));
+
+  addAsyncTask('dart2js_dart',
+      (ctx) =>
+          startProcess(ctx,
+              'dart2js',
+              ['--output-type=dart',
+               '--minify',
+               '-o${deployFolderName}/web/target01/clickme/web/clickme.dart',
+               'web/target01/clickme/web/clickme.dart']));
+
+  addAsyncTask('deploy_gh_pages', (ctx) => branchForDir(ctx, 'master', 'deploy', 'gh-pages'));
+  addAsyncTask('clean', (ctx) => startProcess(ctx, 'rm', ['-rf', deployFolderName]));
+
+  runHop();
 }
 
 void _assertKnownPath() {
