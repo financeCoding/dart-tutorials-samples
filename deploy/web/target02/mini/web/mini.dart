9555559abc8acfd16cedb2d7bd19e7bd482be527
adam <financeCoding@gmail.com>
2013-04-18 20:31:27 -0700

    Adding new dart code

--- deploy/web/target02/mini/web/mini.dart
+++ deploy/web/target02/mini/web/mini.dart
@@ -1,5 +1,1 @@
-import 'dart:html';
-
-void main() {
-  query('#RipVanWinkle').text = 'Wake up, sleepy head!';
-}
+import "dart:html" as g;class h{final  name;const h(this.name);} main(){g.query('#RipVanWinkle').text='Wake up, sleepy head!';}//@ sourceMappingURL=mini.dart.map
--- deploy/web/target03/todo/web/todo.dart
+++ deploy/web/target03/todo/web/todo.dart
@@ -1,17 +1,1 @@
-import 'dart:html';
-
-InputElement toDoInput;
-UListElement toDoList;
-
-void main() {
-  toDoInput = query('#to-do-input');
-  toDoList = query('#to-do-list');
-  toDoInput.onChange.listen(addToDoItem);
-}
-
-void addToDoItem(Event e) {
-  var newToDo = new LIElement();
-  newToDo.text = toDoInput.value;
-  toDoInput.value = '';
-  toDoList.children.add(newToDo);
-}
+import "dart:html" as j;class m{static const  n="Chrome";static const  o="Firefox";static const  p="Internet Explorer";static const  q="Safari";final  l;final  minimumVersion;const m(this.l,[this.minimumVersion]);}class s{final  name;const s(this.name);}var g;var h; main(){g=j.query('#to-do-input');h=j.query('#to-do-list');g.onChange.listen(k);} k( t){var i=new j.LIElement();i.text=g.value;g.value='';h.children.add(i);}//@ sourceMappingURL=todo.dart.map
